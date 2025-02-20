540282f3c34f440e6769887579b8c4da9876d3ac
/mohammedarshad05 <arshff005@gmail.com>
2025-02-20 17:48:31 +0530

    file added

--- car-app/src/App.tsx
+++ car-app/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Car from "./Components/Car";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <header>
+        <h1>Recipe App</h1>
+      </header>
+      <nav className="navbar navbar-expand-lg bg-secondary bg-gradient text-light">
+        <div className="container">
+          <a className="navbar-brand" href="#">Recipe-App</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/about">About Us</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/car">carrr</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <Routes>
+          <Route path="/car" element={<Car />} />
+          
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ car-app/src/Components/Car.tsx
@@ -0,0 +1,8 @@
+function Car() {
+    return (
+        <h2>
+        welcoe to my page
+        </h2>
+    )
+}
+export default Car;
